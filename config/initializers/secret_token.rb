22c3494af8b42ba5109763d16d12fa55937329e3
Diego M. Rodrigues <diegomr86@gmail.com>
2013-05-21 09:36:35 -0300

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClicouBeleza::Application.config.secret_token = 'd45aa38af2981fdcc35934c79a591832f9115ada7ea857a018e2bb5b6bf3b07f2166707e01b49b2af1ca3c84eec5f50fef5768a7daf56d9e6d1ec14d3c49a591'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ClicouBeleza::Application.config.session_store :cookie_store, key: '_Clicou-Beleza_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ClicouBeleza::Application.config.session_store :active_record_store
