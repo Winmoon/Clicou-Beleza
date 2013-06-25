0f6a2823f21998b42251ed807ce85e8b04c1cb43
Winmoon 3 <wendell.palazzo@winmoon.com>
2013-06-25 17:29:02 -0300

    retirado http auth do session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   skip_before_filter :verify_authenticity_token, if: Proc.new { |c| c.request.format == 'application/json' }
-  http_basic_authenticate_with name: 'clicoubeleza', password: 'uyP4RnNMGe4coOm6kIcfAt9E1S8AHK9wwHqPZO9xz7I', if: Proc.new { |c| c.request.format == 'application/json' }
+  #http_basic_authenticate_with name: 'clicoubeleza', password: 'uyP4RnNMGe4coOm6kIcfAt9E1S8AHK9wwHqPZO9xz7I', if: Proc.new { |c| c.request.format == 'application/json' }
 
   def setup
     provider :facebook,
